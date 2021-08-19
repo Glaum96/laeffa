5f27227635f1a54c23acbc00b13137eca5f423d4
Haakon Gunleiksrud <haakongunleiksrud@gmail.com>
2021-08-19 12:32:41 +0200

    add tabs to MainContent

--- frontend/laeffa/src/components/MainContent.tsx
+++ frontend/laeffa/src/components/MainContent.tsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import { KanyeTab } from './KanyeTab';
+import { MemeTab } from './MemeTab';
+import { ChuckNorrisTab } from './ChuckNorrisTab';
+import { DadJokeTab } from './DadJokeTab';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 
 export function MainContent() {
@@ -9,14 +12,22 @@ export function MainContent() {
             <Tabs>
                 <TabList>
                     <Tab>Kanye Be$t</Tab>
-                    <Tab>Title 2</Tab>
+                    <Tab>C.N.</Tab>
+                    <Tab>Hello, Hungry</Tab>
+                    <Tab>Meme</Tab>
                 </TabList>
 
                 <TabPanel>
                     <KanyeTab />
                 </TabPanel>
                 <TabPanel>
-                    <h2>Any content 2</h2>
+                    <ChuckNorrisTab />
+                </TabPanel>
+                <TabPanel>
+                    <DadJokeTab />
+                </TabPanel>
+                <TabPanel>
+                    <MemeTab />
                 </TabPanel>
             </Tabs>
         </div>
