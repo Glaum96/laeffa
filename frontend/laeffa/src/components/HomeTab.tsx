27ced7fd932e99aea195ee17163a68d6ec9504ce
Haakon Gunleiksrud <haakongunleiksrud@gmail.com>
2021-08-19 13:37:03 +0200

    add some styling to tab bar

--- frontend/laeffa/src/components/HomeTab.tsx
+++ frontend/laeffa/src/components/HomeTab.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export function HomeTab() {
     return (
         <div>
-            <img alt="" src={process.env.PUBLIC_URL + '/wordart.png'} />
+            <img alt="" src={process.env.PUBLIC_URL + '/wordart.png'} width="100%"/>
         </div>
     )
 }
