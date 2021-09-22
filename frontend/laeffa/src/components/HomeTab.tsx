cb25eba0e4d421b739e5d553d6b3dfca9087582c
Haakon Gunleiksrud <haakongunleiksrud@gmail.com>
2021-09-22 10:06:59 +0200

    news and data

--- frontend/laeffa/src/components/HomeTab.tsx
+++ frontend/laeffa/src/components/HomeTab.tsx
@@ -4,6 +4,31 @@ export function HomeTab() {
     return (
         <div>
             <img alt="" src={process.env.PUBLIC_URL + '/wordart.png'} width="100%"/>
+            <div>
+                <h1>Ny tillitslæff er valgt</h1>
+                <p>Gratulerer til Herman med vervet som tillitsvalgt i Læffa. Herman sier ydmykt til laeffa.no 
+                    at det er en stor ære å skulle ta på seg det tunge ansvaret med å være ansvarlig for 
+                    gjengen med gissegasser + Adrian.
+                </p>
+                <h5>Ber om ro til å lage tillitsvalgterklæring</h5>
+                <p>
+                    Herman ber om ro fra pressen, og særlig den pågående laeffa.no-journalisten som ønsker å vite hvilke
+                    endringer læffene kan vente seg i den kommende tillitsvalgtperioden. Herman har booket 
+                    konferanselokalene på Delta-kontoret i 2 uker for sonderinger med seg selv før han vil fortelle om
+                    hva han har kommet frem til. "Forskjellene mellom Læffer har økt og økt, og nå trenger vi en ny kurs!"
+                    Dette skriver Herman entusiastisk i en sms til laeffa.no før han går inn i sonderingene.
+                    Vi ønsker han lykke til.
+                </p>
+                <img alt="" src={process.env.PUBLIC_URL + '/herman.jpg'} width="30%"/>
+
+            </div>
+            <div>
+                <h1>Karen raser mot fest på Møllenberg -Da jeg var ung drakk vi kun sølevann og sæd!</h1>
+                <p>laeffa.no kan avsløre at det bor studenter på Møllenberg. Nyhetene kom som et sjokk på 
+                    Trondheims aldrende befolkning. Særlig journalistene i Adressavisen.
+                </p>
+                <img alt="" src={process.env.PUBLIC_URL + '/mollenberg.PNG'} width="30%"/>
+            </div>
         </div>
     )
 }
--- frontend/laeffa/src/components/VingeSpennTab.tsx
+++ frontend/laeffa/src/components/VingeSpennTab.tsx
@@ -28,35 +28,54 @@ export function VingeSpennTab() {
                     <tr>
                         <th>Navn</th>
                         <th>Høyde (cm)</th>
-                        <th>Vingespenn (m)</th>
+                        <th>Vingespenn (cm)</th>
+                        <th>Differanse (cm)</th>
                     </tr>
                     <tr>
                         <td>Eskild</td>
-                        <td> mindre enn Haakon</td>
-                        <td>?</td>
+                        <td>192</td>
+                        <td>194</td>
+                        <td>2</td>
                     </tr>
                     <tr>
                         <td>Haakon</td>
                         <td>201</td>
-                        <td>? + 10</td>
+                        <td>202</td>
+                        <td>1</td>
                     </tr>
                     <tr>
                         <td>Adrian</td>
-                        <td>?</td>
-                        <td>?</td>
+                        <td>175,5</td>
+                        <td>175,5</td>
+                        <td>0</td>
                     </tr>
                     <tr>
                         <td>Eivind</td>
-                        <td>?</td>
-                        <td>?</td>
+                        <td>176</td>
+                        <td>182</td>
+                        <td>6 (!)</td>
                     </tr>
                     <tr>
                         <td>Herman</td>
-                        <td>?</td>
-                        <td>?</td>
+                        <td>6969</td>
+                        <td>42</td>
+                        <td>-6927</td>
                     </tr>
                     <tr>
                         <td>Arnstein</td>
+                        <td>191,5</td>
+                        <td>190</td>
+                        <td>-1,5</td>
+                    </tr>
+                    <tr>
+                        <td>Helene</td>
+                        <td>168</td>
+                        <td>162,5</td>
+                        <td>-5,5</td>
+                    </tr>
+                    <tr>
+                        <td>Anna</td>
+                        <td>?</td>
                         <td>?</td>
                         <td>?</td>
                     </tr>
