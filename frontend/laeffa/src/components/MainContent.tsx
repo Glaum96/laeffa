57cc33eaf9a8f6db569fa9820ec18fcfa92a4d33
Haakon Gunleiksrud <haakogun@stud.ntnu.no>
2022-02-02 09:24:05 +0100

    feat: new quotes tab

--- frontend/laeffa/src/components/MainContent.tsx
+++ frontend/laeffa/src/components/MainContent.tsx
@@ -1,44 +1,47 @@
-import { HomeTab } from './HomeTab';
-import { KanyeTab } from './KanyeTab';
-import { MemeTab } from './MemeTab';
-import { ChuckNorrisTab } from './ChuckNorrisTab';
-import { DadJokeTab } from './DadJokeTab';
-import { VingeSpennTab } from './VingeSpennTab';
-import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import { HomeTab } from "./HomeTab";
+import { KanyeTab } from "./KanyeTab";
+import { MemeTab } from "./MemeTab";
+import { ChuckNorrisTab } from "./ChuckNorrisTab";
+import { DadJokeTab } from "./DadJokeTab";
+import { QuotesTab } from "./Quotes";
+import { VingeSpennTab } from "./VingeSpennTab";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 
 export function MainContent() {
-    
-    return (
-        <div >
-            <Tabs>
-                <TabList>
-                    <Tab>Home</Tab>
-                    <Tab>Kanye Be$t</Tab>
-                    <Tab>C.N.</Tab>
-                    <Tab>Hello, Hungry</Tab>
-                    <Tab>Meme</Tab>
-                    <Tab>Secret</Tab>
-                </TabList>
-
-                <TabPanel>
-                    <HomeTab />
-                </TabPanel>
-                <TabPanel>
-                    <KanyeTab />
-                </TabPanel>
-                <TabPanel>
-                    <ChuckNorrisTab />
-                </TabPanel>
-                <TabPanel>
-                    <DadJokeTab />
-                </TabPanel>
-                <TabPanel>
-                    <MemeTab />
-                </TabPanel>
-                <TabPanel>
-                    <VingeSpennTab />
-                </TabPanel>
-            </Tabs>
-        </div>
-    )
+  return (
+    <div>
+      <Tabs>
+        <TabList>
+          <Tab>Home</Tab>
+          <Tab>Quotes</Tab>
+          <Tab>Kanye Be$t</Tab>
+          <Tab>C.N.</Tab>
+          <Tab>Hello, Hungry</Tab>
+          <Tab>Meme</Tab>
+          <Tab>Secret</Tab>
+        </TabList>
+        <TabPanel>
+          <HomeTab />
+        </TabPanel>
+        <TabPanel>
+          <QuotesTab />
+        </TabPanel>
+        <TabPanel>
+          <KanyeTab />
+        </TabPanel>
+        <TabPanel>
+          <ChuckNorrisTab />
+        </TabPanel>
+        <TabPanel>
+          <DadJokeTab />
+        </TabPanel>
+        <TabPanel>
+          <MemeTab />
+        </TabPanel>
+        <TabPanel>
+          <VingeSpennTab />
+        </TabPanel>
+      </Tabs>
+    </div>
+  );
 }
---
+++ frontend/laeffa/src/components/Quotes.tsx
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from "react";
+
+export function QuotesTab() {
+  return (
+    <div className="main-content">
+      <h1>Sitater</h1>
+      <p className="ingress">Adrian - 31.01.22</p>
+      <p>
+        "Anna er det beste du har bidratt med til dette kollektivet, Arnstein."
+      </p>
+    </div>
+  );
+}
